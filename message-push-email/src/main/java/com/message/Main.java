c3b23426515bd419d8b83dfb3c35aa39aa341bc1
kang <1115610574@qq.com>
2021-08-04 10:39:49 +0800

    refactor: Main.java to com.message package

--- message-push-email/src/main/java/com/message/Main.java
+++ message-push-email/src/main/java/com/message/Main.java
@@ -1,4 +1,4 @@
-package com.message.email;
+package com.message;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- message-push-email/src/test/java/com/message/MainTest.java
+++ message-push-email/src/test/java/com/message/MainTest.java
@@ -1,4 +1,4 @@
-package com.message.email;
+package com.message;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
