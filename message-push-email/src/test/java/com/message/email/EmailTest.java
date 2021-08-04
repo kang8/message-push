ab191d85a7e1f5411ba46114a2967fefa3d30528
kang <1115610574@qq.com>
2021-08-04 14:21:14 +0800

    refactor: use application to store recipient(收件人)

--- message-push-email/src/test/java/com/message/email/EmailTest.java
+++ message-push-email/src/test/java/com/message/email/EmailTest.java
@@ -6,13 +6,16 @@ import javax.mail.MessagingException;
 import javax.mail.internet.AddressException;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = Email.class)
 public class EmailTest {
+  @Value("${mail.recipient}")
+  private String recipient;
 
   @Test
   void testSendFor3Param() throws AddressException, MessagingException {
-    Email.send(Collections.singletonList("@qq.com"), "test send email", "I want to send email");
+    Email.send(Collections.singletonList(recipient), "test send email", "I want to send email");
   }
 }
