30262a759a17654a3e25c83a52c94baa42cf6df5
kang <1115610574@qq.com>
2021-08-04 18:51:16 +0800

    build: add child module is message-push-wechat

---
+++ message-push-wechat/src/main/java/com/message/wechat/WeChat.java
@@ -0,0 +1,11 @@
+package org.kang.example;
+
+/**
+ * @author kang
+ * @date 2021/7/28 10:22
+ */
+public class WeChat {
+    public static void main(String[] args) {
+        System.out.println("Hello Maven Example");
+    }
+}
