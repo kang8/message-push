a598dd265adb879180a301e6a81a0179dc70682c
kang <1115610574@qq.com>
2021-08-05 14:48:58 +0800

    feat: add wechat properties

---
+++ message-push-wechat/src/main/java/com/message/wechat/config/WeChatProperties.java
@@ -0,0 +1,24 @@
+package com.message.wechat.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "wechat")
+public class WeChatProperties {
+    /**
+     * 企业 ID
+     */
+    private String corpId;
+    /**
+     * 应用密钥
+     */
+    private String corpSecret;
+    /**
+     * 应用 Id
+     */
+    private int agentId;
+}
