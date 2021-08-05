99d53d7fad440b548dbaff22ff0e9eeb463fe965
kang <1115610574@qq.com>
2021-08-05 15:18:12 +0800

    feat: add SpringBootApplication running

--- message-push-wechat/src/main/java/com/message/wechat/WeChat.java
+++ message-push-wechat/src/main/java/com/message/wechat/WeChat.java
@@ -2,10 +2,6 @@ package com.message.wechat;
 
 /**
  * @author kang
- * @date 2021/7/28 10:22
  */
 public class WeChat {
-    public static void main(String[] args) {
-        System.out.println("Hello Maven Example");
-    }
 }
