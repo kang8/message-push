48c7a771bf103145828e8c585015aa84969df618
kang <1115610574@qq.com>
2021-08-05 19:14:38 +0800

    feat: add api for wechat

--- message-push-wechat/src/main/java/com/message/wechat/WeChat.java
+++ message-push-wechat/src/main/java/com/message/wechat/WeChat.java
@@ -1,7 +1,40 @@
 package com.message.wechat;
 
+import com.message.wechat.entity.BaseMessage;
+import com.message.wechat.entity.MessageResponse;
+import com.message.wechat.entity.TextMessage;
+import com.message.wechat.entity.TokenResponse;
+import com.message.wechat.service.WeChatService;
+
+import retrofit2.Call;
+
 /**
  * @author kang
  */
 public class WeChat {
+
+    public void sendTextMessage(String accessToken, TextMessage message) {
+
+    }
+
+    public String getAccessToken(String cropId, String cropSrcret) {
+        // TODO:
+        return new String("");
+    }
+
+    public WeChatService setUpWeChatService() {
+        return new WeChatService() {
+            @Override
+            public Call<TokenResponse> getAccessToken(String cropId, String cropSecret) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+
+            @Override
+            public Call<MessageResponse> sendMessage(String accessToken, BaseMessage message) {
+                // TODO Auto-generated method stub
+                return null;
+            }
+        };
+    }
 }
---
+++ message-push-wechat/src/main/java/com/message/wechat/service/WeChatService.java
@@ -0,0 +1,19 @@
+package com.message.wechat.service;
+
+import com.message.wechat.entity.BaseMessage;
+import com.message.wechat.entity.MessageResponse;
+import com.message.wechat.entity.TokenResponse;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Query;
+
+public interface WeChatService {
+    @GET("/cgi-bin/gettoken")
+    Call<TokenResponse> getAccessToken(@Query("corpid") String cropId, @Query("corpsecret") String cropSecret);
+
+    @POST("/cgi-bin/message/send")
+    Call<MessageResponse> sendMessage(@Query("access_token") String accessToken, @Body BaseMessage message);
+}
