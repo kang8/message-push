22c8b3808f59b2b141db8d4bf82d08670ba4d72a
kang <1115610574@qq.com>
2021-08-06 14:05:52 +0800

    feat: complete WeChat getAccessToken
    - normalize method to static method
    - add test method for getAccessToken

--- message-push-wechat/src/main/java/com/message/wechat/WeChat.java
+++ message-push-wechat/src/main/java/com/message/wechat/WeChat.java
@@ -1,40 +1,51 @@
 package com.message.wechat;
 
-import com.message.wechat.entity.BaseMessage;
-import com.message.wechat.entity.MessageResponse;
+import java.io.IOException;
+import java.util.Objects;
+
+import com.message.wechat.config.WeChatProperties;
 import com.message.wechat.entity.TextMessage;
 import com.message.wechat.entity.TokenResponse;
 import com.message.wechat.service.WeChatService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 /**
  * @author kang
  */
+@Component
 public class WeChat {
 
-    public void sendTextMessage(String accessToken, TextMessage message) {
+    private static WeChatProperties weChatProperties;
+
+    @Autowired
+    private WeChat(WeChatProperties weChatProperties) {
+        WeChat.weChatProperties = weChatProperties;
+    }
+
+    public static void sendTextMessage(String accessToken, TextMessage message) {
 
     }
 
-    public String getAccessToken(String cropId, String cropSecret) {
-        // TODO:
-        return new String("");
+    public static String getAccessToken(String cropId, String cropSecret) throws IOException {
+        WeChatService weChatService = setUpWeChatService();
+
+        Call<TokenResponse> tokenRes = weChatService.getAccessToken(weChatProperties.getCorpId(),
+                weChatProperties.getCorpSecret());
+        return Objects.requireNonNull(tokenRes.execute().body()).getAccess_token();
     }
 
-    public WeChatService setUpWeChatService() {
-        return new WeChatService() {
-            @Override
-            public Call<TokenResponse> getAccessToken(String cropId, String cropSecret) {
-                // TODO Auto-generated method stub
-                return null;
-            }
+    public static WeChatService setUpWeChatService() {
 
-            @Override
-            public Call<MessageResponse> sendMessage(String accessToken, BaseMessage message) {
-                // TODO Auto-generated method stub
-                return null;
-            }
-        };
+        Retrofit retrofit = new Retrofit.Builder().baseUrl(weChatProperties.getBaseUrl())
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        return retrofit.create(WeChatService.class);
+
     }
 }
--- message-push-wechat/src/test/java/com/message/wechat/WeChatTest.java
+++ message-push-wechat/src/test/java/com/message/wechat/WeChatTest.java
@@ -1,17 +1,25 @@
 package com.message.wechat;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+import java.io.IOException;
+
 import com.message.IntegrationWeChatText;
+import com.message.wechat.config.WeChatProperties;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class WeChatTest extends IntegrationWeChatText {
 
-    @Test
-    void testGetAccessToken() {
-        WeChat wechat = new WeChat();
-        String accessToken = wechat.getAccessToken("cropId", "cropSecret");
+    @Autowired
+    private WeChatProperties wechatProperties;
 
-        assertEquals("", accessToken);
+    @Test
+    void testGetAccessToken() throws IOException {
+        String accessToken = WeChat.getAccessToken(wechatProperties.getCorpId(), wechatProperties.getCorpSecret());
+
+        assertFalse(accessToken.isEmpty());
     }
 
     @Test
