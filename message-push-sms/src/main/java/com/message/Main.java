1917c04984cd19bfcf68cd793c360eae5aca2fcc
kang <1115610574@qq.com>
2021-08-10 14:46:38 +0800

    feat: add sms module

---
+++ message-push-sms/src/main/java/com/message/Main.java
@@ -0,0 +1,15 @@
+package com.message;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kang
+ * @date 2021/7/28 10:22
+ */
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
---
+++ message-push-sms/src/test/java/com/message/IntegrationSmsTest.java
@@ -0,0 +1,18 @@
+package com.message;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * @author kang
+ * @date 2021/7/28 11:22
+ */
+@SpringBootTest
+class IntegrationSmsTest {
+    @Test
+    public void testBooleanTrue() {
+        assertTrue(Boolean.TRUE);
+    }
+}
